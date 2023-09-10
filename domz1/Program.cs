4f33434cc9d41dffeb38871412e3f2076df2ecc1
AnnBelova <anutik-89@tut.by>
2023-09-10 04:16:40 +0300

    задача 1

--- domz1/Program.cs
+++ domz1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Пользователь вводит с клавиатуры кол-во чисел. Посчитать, сколько чисел  >0 ввел пользователь:
+int Input(string message)
+{
+    System.Console.WriteLine(message);
+    return int.Parse(Console.ReadLine());
+}
+int Itog(int num)
+{
+    int count=0;
+    for (int i = 1; i <= num; i++)
+    {
+        if (int.Parse(Console.ReadLine()) > 0)
+        {
+            count = count + 1;
+        }
+    }
+        return count;
+}
+
+int m = Input("Введите колличество элементов: ");
+int itog = Itog(m);
+
+System.Console.WriteLine($"Чисел больше нуля {itog}  ");
